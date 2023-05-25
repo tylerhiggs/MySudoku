08f776da8d56e532d1a18b890c7111777b900c5a
tylerhiggs <thiggs@mit.edu>
2023-05-25 14:00:00 -0500

    Initial Commit

---
+++ MySudoku/MySudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySudokuApp.swift
+//  MySudoku
+//
+//  Created by Tyler Higgs on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MySudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
